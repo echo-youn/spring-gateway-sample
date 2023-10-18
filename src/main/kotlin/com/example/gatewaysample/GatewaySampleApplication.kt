37e7564919ce1d5d8091402164a692c6da66a963
echo <echo@nexon.co.kr>
2023-10-18 14:26:49 +0900

    project init

---
+++ src/main/kotlin/com/example/gatewaysample/GatewaySampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gatewaysample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewaySampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewaySampleApplication>(*args)
+}
